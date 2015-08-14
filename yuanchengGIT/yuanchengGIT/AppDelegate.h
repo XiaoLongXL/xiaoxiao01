742ef23dbbc810b19bc787c960a376e9848b67ef
xiaoxiao <372322155@qq.com>
2015-08-14 10:05:07 +0800

    添加工程

---
+++ yuanchengGIT/yuanchengGIT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  yuanchengGIT
+//
+//  Created by xiaolong on 15/8/14.
+//  Copyright (c) 2015年 XXL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ yuanchengGIT/yuanchengGIT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  yuanchengGIT
+//
+//  Created by xiaolong on 15/8/14.
+//  Copyright (c) 2015年 XXL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
